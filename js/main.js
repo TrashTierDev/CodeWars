dac70bb47c85f6e525f91263aef203c2e21c6a3e
TrashTierDev <lettersforyou11@gmail.com>
2022-04-11 15:12:36 -0400

    another challenge added

--- js/main.js
+++ js/main.js
@@ -1,12 +1,23 @@
 // Write a function named repeater() that takes two arguments (a string and a number), and returns a new string where the input string is repeated that many times.
 
 
-function repeater(string, n){
-   let result ="";
+// function repeater(string, n){
+//    let result ="";
 
-   for (let i=1; i <= n; i++){
-       result += string;
-   }
-   console.log(result);
+//    for (let i=1; i <= n; i++){
+//        result += string;
+//    }
+//    console.log(result);
+// }
+//   repeater('x', 5)
+
+// Write a function that returns a string in which firstname is swapped with last name.
+
+function nameShuffler(str){
+    let arr= str.split(" ")
+    let reversearr= arr.reverse()
+    let joinarr= reversearr.join(" ")
+    return joinarr
 }
-  repeater('x', 5)
+
+console.log (nameShuffler('steven phipps'))
