ec438847558f8355d7ebac40a5d66907dfc8dbd6
TrashTierDev <lettersforyou11@gmail.com>
2022-04-13 11:28:03 -0400

    added more code war challenges

--- js/main.js
+++ js/main.js
@@ -13,11 +13,20 @@
 
 // Write a function that returns a string in which firstname is swapped with last name.
 
-function nameShuffler(str){
-    let arr= str.split(" ")
-    let reversearr= arr.reverse()
-    let joinarr= reversearr.join(" ")
-    return joinarr
-}
+// function nameShuffler(str){
+//     let arr= str.split(" ")
+//     let reversearr= arr.reverse()
+//     let joinarr= reversearr.join(" ")
+//     return joinarr
+// }
 
-console.log (nameShuffler('steven phipps'))
+// console.log (nameShuffler('steven phipps'))
+
+// Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
+
+function sum (numbers) {
+
+return numbers.reduce((a, b) => a + b, 0)
+    
+};
+console.log(sum([1, 5.2, 4, 0, -1]))
