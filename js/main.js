78e11d064567b81e28a841ce824aa82cca802c9d
TrashTierDev <lettersforyou11@gmail.com>
2022-04-14 08:49:17 -0400

    added more challs

--- js/main.js
+++ js/main.js
@@ -24,9 +24,36 @@
 
 // Write a function that takes an array of numbers and returns the sum of the numbers. The numbers can be negative or non-integer. If the array does not contain any numbers then you should return 0.
 
-function sum (numbers) {
+// function sum (numbers) {
 
-return numbers.reduce((a, b) => a + b, 0)
+// return numbers.reduce((a, b) => a + b, 0)
     
-};
-console.log(sum([1, 5.2, 4, 0, -1]))
+// };
+// console.log(sum([1, 5.2, 4, 0, -1]))
+
+
+// Can you find the needle in the haystack?
+
+// Write a function findNeedle() that takes an array full of junk but containing one "needle"
+
+// After your function finds the needle it should return a message (as a string) that says:
+
+// "found the needle at position " plus the index it found the needle, so:
+
+// let haystack=(['hay', 'junk', 'hay', 'hay', 'moreJunk', 'needle', 'randomJunk'])
+
+// function findNeedle(haystack) {
+//  let newNeedle = haystack.indexOf('needle', 0)
+//  return ("found the needle at position" + " " + newNeedle)
+ 
+// }
+
+
+function lovefunc(flower1, flower2){
+  if (flower1 % 2 === 0 && flower2 % 2 === 1 || flower1 % 2 === 1 && flower2 % 2 === 0){
+    return 'true'
+  } else
+  return 'false'
+}
+
+console.log(lovefunc(0,0))
